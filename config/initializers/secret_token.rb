263a40962862d3bbba65e468e27d29764f7125c3
xiaopangpang <xiangning.li.cornell@outlook.com>
2013-06-08 23:02:21 -0700

    first ocmmit

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Buty::Application.config.secret_token = 'ed6c34576782ed5106904c5cad092f03f609fb94f63fdb2e3f2044b4e629c6c6c06ca050f092864367efd5d7e0e379d324b99cc3868f72086fdb79892fb30afd'
